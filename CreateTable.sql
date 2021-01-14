2c7d5416f4fc0f3c93609a2f966509cc9c9c6d94
julianameo <77309679+julianameo@users.noreply.github.com>
2021-01-14 15:47:50 -0500

    Add files via upload

---
+++ CreateTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [Supplies] (
+	SupplyID INT IDENTITY(1, 1),
+	SupplyName varchar(100),
+	SupplyUnitType INT, 
+	SupplyUnits INT, 
+	Status INT)
---
+++ InsertIntoTable.sql
@@ -0,0 +1,2 @@
+INSERT INTO CurrentSupplyInventory (TavernID,SupplyID,DateUpdated,TotalCount)
+VALUES ('3',6,1/11/2021,25)
